6de725f061a71affbe7e0244fae94705ffae2d3d
hawky358 <hawkway@gmail.com>
2023-06-18 19:55:57 +0200

    Update constants layout

--- custom_components/ids_hyyp/const.py
+++ custom_components/ids_hyyp/const.py
@@ -8,12 +8,13 @@ MODEL = "Hyyp"
 CONF_PKG = "pkg"
 
 # Package types
-PKG_ADT_SECURE_HOME = "za.co.adt.securehome.android"
 PKG_IDS_HYYP = "com.hyyp247.home"
+PKG_ADT_SECURE_HOME = "za.co.adt.securehome.android"
 
 # Pacakage Alias
-PKG_ADT_ALIAS = "ADT Securehome (Untested)"
 PGK_IDS_HYYP_ALIAS = "IDS Hyyp"
+PKG_ADT_ALIAS = "ADT Securehome (Untested)"
+
 
 # Defaults
 DEFAULT_TIMEOUT = 25
